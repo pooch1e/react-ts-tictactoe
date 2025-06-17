c86012c3f110921855a3f254c54ea87b9f6a3515
Joel Kram <joelnskram@gmail.com>
2025-06-17 16:20:53 +0100

    creating boardstate

---
+++ react-ttt-tsx/src/components/Main.tsx
@@ -0,0 +1,11 @@
+import { Gameboard } from './Gameboard'
+// will track score and who won in state
+export const Main = () => {
+
+  return (
+    <main>
+      <h2>main area</h2>
+      <Gameboard />
+    </main>
+  )
+}

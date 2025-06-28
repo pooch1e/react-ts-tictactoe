353186a4938c24cd9eeac6a3c88d33e089d26716
Joel Kram <joelnskram@gmail.com>
2025-06-28 22:06:08 +0100

    rename project and using https://scottlilly.com/learn-c-by-building-a-simple-rpg-index/lesson-05-1-creating-objects-from-classes/

---
+++ react-ttt-tsx/src/components/Game.tsx
@@ -0,0 +1,12 @@
+import { PlayerClass } from './PlayerClass';
+
+const player1 = new PlayerClass(10, 10, 20, 0, 1);
+
+export const Game = () => {
+  return (
+    <>
+      <h1>Game Space</h1>
+      <div></div>
+    </>
+  );
+};
